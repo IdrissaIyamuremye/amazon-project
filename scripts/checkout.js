fc6efacf2ea46b75d5a2214a96081a112d407a38
Idrissa Iyamuremye <idrissa.iyamuremye@ozu.edu.tr>
2025-07-27 16:53:57 +0300

    Delete product form HTML when deleted

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -98,5 +98,8 @@ document.querySelectorAll(".js-delete-link").forEach((link)=>{
     const productId = link.dataset.productId;
     removeFromCart(productId)
     console.log(cart)
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove()
+
   })
 })
