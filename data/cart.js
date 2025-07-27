5c7ecada8121a354a08e6e7cd4bcc541d97caa8d
Idrissa Iyamuremye <idrissa.iyamuremye@ozu.edu.tr>
2025-07-27 20:22:20 +0300

    Make delivery option iteractive

--- data/cart.js
+++ data/cart.js
@@ -3,10 +3,12 @@ if(!cart){
     
  cart= [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: "1"
 },{
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: "2"
 }]
 };
 export function addToCart(productId) {
@@ -22,7 +24,8 @@ export function addToCart(productId) {
         }else{
             cart.push({
             productId:productId,
-            quantity: 1
+            quantity: 1,
+            deliveryOptionId: "1"
         })
             
         }
@@ -40,4 +43,15 @@ export function removeFromCart(productId){
     })
     cart = newCart
     saveToStorage()
+    }
+    export function updateDeliveryOption(productId, deliveryOptionId){
+        let matchingcartItem;
+        cart.forEach((cartItem)=>{
+            if(productId===cartItem.productId){
+            matchingcartItem = cartItem;
+            }
+
+        });
+        matchingcartItem.deliveryOptionId = deliveryOptionId
+        saveToStorage();
     }
